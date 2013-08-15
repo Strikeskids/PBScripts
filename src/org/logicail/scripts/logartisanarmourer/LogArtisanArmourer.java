43ea537d151c5857806aabae9dbf24e0f6337657
Logicail <logicail@mike-nightingale.co.uk>
2013-08-15 23:12:51 +0100

    2.02 (test if hidden works)

--- src/org/logicail/scripts/logartisanarmourer/LogArtisanArmourer.java
+++ src/org/logicail/scripts/logartisanarmourer/LogArtisanArmourer.java
@@ -27,7 +27,7 @@ import java.util.ArrayList;
 @Manifest(
 		name = "LogArtisanArmourer",
 		description = "Cheap smithing at Artisans Workshop",
-		version = 2.01,
+		version = 2.02,
 		authors = {"Logicail"},
 		hidden = true,
 		website = "http://www.powerbot.org/community/topic/704413-logartisan-artisan-armourer-cheap-smither/")
