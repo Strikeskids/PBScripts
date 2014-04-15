4e9db4940a346ed903e5cfe13cba9eea6d3307e9
Logicail <logicail@mike-nightingale.co.uk>
2014-04-15 19:20:18 +0100

    Larger repaint

--- src/org/logicail/rsbot/scripts/testing/FarmingPaint.java
+++ src/org/logicail/rsbot/scripts/testing/FarmingPaint.java
@@ -33,41 +33,40 @@ public class FarmingPaint extends IClientAccessor implements PaintListener {
 		int y = 50;
 
 		g2.setColor(COLOUR_BACKGROUND);
-		g2.fillRect(x - 3, y - 3, 198, 42);
+		g2.fillRect(x - 3, y - 3, 199, 47);
 		g2.setColor(COLOUR_BACKGROUND_BORDER);
-		g2.drawRect(x - 3, y - 3, 198, 42);
+		g2.drawRect(x - 3, y - 3, 199, 47);
 
-		HerbEnum.FALADOR.herb(ctx).repaint(g2, x + 26, y);
 		AllotmentEnum.FALADOR_N.allotment(ctx).repaint(g2, x, y);
-		AllotmentEnum.FALADOR_S.allotment(ctx).repaint(g2, x + 17, y + 14);
+		HerbEnum.FALADOR.herb(ctx).repaint(g2, x + 32, y);
+		AllotmentEnum.FALADOR_S.allotment(ctx).repaint(g2, x + 20, y + 16);
 		x += 46;
 		AllotmentEnum.CATHERBY_N.allotment(ctx).repaint(g2, x, y);
-		HerbEnum.CATHERBY.herb(ctx).repaint(g2, x + 26, y + 14);
-		AllotmentEnum.CATHERBY_S.allotment(ctx).repaint(g2, x, y + 24);
+		HerbEnum.CATHERBY.herb(ctx).repaint(g2, x + 32, y + 16);
+		AllotmentEnum.CATHERBY_S.allotment(ctx).repaint(g2, x, y + 28);
 		x += 46;
 		AllotmentEnum.ARDOUGNE_N.allotment(ctx).repaint(g2, x, y);
-		HerbEnum.ARDOUGNE.herb(ctx).repaint(g2, x + 26, y + 14);
-		AllotmentEnum.ARDOUGNE_S.allotment(ctx).repaint(g2, x, y + 24);
+		HerbEnum.ARDOUGNE.herb(ctx).repaint(g2, x + 32, y + 16);
+		AllotmentEnum.ARDOUGNE_S.allotment(ctx).repaint(g2, x, y + 28);
 		x += 46;
-		HerbEnum.PORT_PHASMATYS.herb(ctx).repaint(g2, x + 26, y);
 		AllotmentEnum.PORT_PHASMATYS_N.allotment(ctx).repaint(g2, x, y);
-		AllotmentEnum.PORT_PHASMATYS_S.allotment(ctx).repaint(g2, x + 17, y + 14);
+		HerbEnum.PORT_PHASMATYS.herb(ctx).repaint(g2, x + 32, y);
+		AllotmentEnum.PORT_PHASMATYS_S.allotment(ctx).repaint(g2, x + 20, y + 16);
 		x += 46;
 		HerbEnum.TROLLHEIM.herb(ctx).repaint(g2, x, y);
 
 		x = 251;
 
 		g2.setColor(COLOUR_BACKGROUND);
-		g2.fillRect(x - 3, y - 3, 75, CROP_STATES.length * 15 + 3);
+		g2.fillRect(x - 3, y - 3, 75, CROP_STATES.length * 15);
 		g2.setColor(COLOUR_BACKGROUND_BORDER);
-		g2.drawRect(x - 3, y - 3, 75, CROP_STATES.length * 15 + 3);
-
+		g2.drawRect(x - 3, y - 3, 75, CROP_STATES.length * 15);
 
 		for (CropState state : CROP_STATES) {
 			g2.setColor(state.color());
-			g2.fillRect(x, y, 7, 7);
+			g2.fillRect(x, y, 9, 9);
 			g2.setColor(Color.gray);
-			g2.drawRect(x, y, 7, 7);
+			g2.drawRect(x, y, 9, 9);
 
 			g2.setColor(Color.black);
 			g2.drawString(state.toString(), x + 15, y + 10);
