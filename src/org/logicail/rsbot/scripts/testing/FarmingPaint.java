499e4d0efa83eb04ac5b5642448fc2db447da9b2
Logicail <logicail@mike-nightingale.co.uk>
2014-04-16 01:10:15 +0100

    Add flower to paint

--- src/org/logicail/rsbot/scripts/testing/FarmingPaint.java
+++ src/org/logicail/rsbot/scripts/testing/FarmingPaint.java
@@ -4,6 +4,7 @@ import org.logicail.rsbot.scripts.framework.context.IClientAccessor;
 import org.logicail.rsbot.scripts.framework.context.IClientContext;
 import org.logicail.rsbot.scripts.framework.context.providers.farming.CropState;
 import org.logicail.rsbot.scripts.framework.context.providers.farming.enums.AllotmentEnum;
+import org.logicail.rsbot.scripts.framework.context.providers.farming.enums.FlowerEnum;
 import org.logicail.rsbot.scripts.framework.context.providers.farming.enums.HerbEnum;
 import org.powerbot.script.PaintListener;
 
@@ -39,18 +40,22 @@ public class FarmingPaint extends IClientAccessor implements PaintListener {
 
 		AllotmentEnum.FALADOR_N.object(ctx).repaint(g2, x, y);
 		HerbEnum.FALADOR.object(ctx).repaint(g2, x + 32, y);
+		FlowerEnum.FALADOR.object(ctx).repaint(g2, x + 16, y + 16);
 		AllotmentEnum.FALADOR_S.object(ctx).repaint(g2, x + 20, y + 16);
 		x += 46;
 		AllotmentEnum.CATHERBY_N.object(ctx).repaint(g2, x, y);
 		HerbEnum.CATHERBY.object(ctx).repaint(g2, x + 32, y + 16);
+		FlowerEnum.CATHERBY.object(ctx).repaint(g2, x + 16, y + 16);
 		AllotmentEnum.CATHERBY_S.object(ctx).repaint(g2, x, y + 28);
 		x += 46;
 		AllotmentEnum.ARDOUGNE_N.object(ctx).repaint(g2, x, y);
 		HerbEnum.ARDOUGNE.object(ctx).repaint(g2, x + 32, y + 16);
+		FlowerEnum.ARDOUGNE.object(ctx).repaint(g2, x + 16, y + 16);
 		AllotmentEnum.ARDOUGNE_S.object(ctx).repaint(g2, x, y + 28);
 		x += 46;
 		AllotmentEnum.PORT_PHASMATYS_N.object(ctx).repaint(g2, x, y);
 		HerbEnum.PORT_PHASMATYS.object(ctx).repaint(g2, x + 32, y);
+		FlowerEnum.PORT_PHASMATYS.object(ctx).repaint(g2, x + 16, y + 16);
 		AllotmentEnum.PORT_PHASMATYS_S.object(ctx).repaint(g2, x + 20, y + 16);
 		x += 46;
 		HerbEnum.TROLLHEIM.object(ctx).repaint(g2, x, y);
