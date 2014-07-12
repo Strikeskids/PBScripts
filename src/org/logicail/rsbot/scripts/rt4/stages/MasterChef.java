d13c840e70346f580268dc57dbf01f8b4321a2a5
Logicail <logicail@mike-nightingale.co.uk>
2014-07-12 11:15:59 +0100

    Chef avoid wait timeout walking to entrance

--- src/org/logicail/rsbot/scripts/rt4/stages/MasterChef.java
+++ src/org/logicail/rsbot/scripts/rt4/stages/MasterChef.java
@@ -4,6 +4,7 @@ import com.logicail.wrappers.ObjectDefinition;
 import org.logicail.rsbot.scripts.framework.context.rt4.IClientContext;
 import org.logicail.rsbot.scripts.rt4.OSTutorialIsland;
 import org.powerbot.script.Condition;
+import org.powerbot.script.Random;
 import org.powerbot.script.rt4.GameObject;
 import org.powerbot.script.rt4.Interactive;
 import org.powerbot.script.rt4.Item;
@@ -58,14 +59,24 @@ public class MasterChef extends Talker {
 		ctx.inventory.deselect();
 
 		if (ctx.chat.visible("Follow the path until you get to the door with the yellow arrow")) {
-			final GameObject door = ctx.objects.select().select(ObjectDefinition.name(ctx, "Door")).each(Interactive.doSetBounds(new int[]{0, 32, -240, 0, 0, 120})).nearest().poll();
-			if (door.valid() && ctx.camera.prepare(door) && door.click("Open", "Door")) {
-				Condition.wait(new Callable<Boolean>() {
-					@Override
-					public Boolean call() throws Exception {
-						return ctx.chat.visible("Talk to the chef indicated.");
-					}
-				}, 200, 40);
+			if (!ctx.players.local().inMotion() || ctx.movement.distance(ctx.movement.destination(), ctx.players.local()) < Random.nextInt(2, 5)) {
+				final GameObject door = ctx.objects.select().select(ObjectDefinition.name(ctx, "Door")).each(Interactive.doSetBounds(new int[]{0, 32, -240, 0, 0, 120})).nearest().poll();
+				if (door.valid() && ctx.camera.prepare(door) && door.click("Open", "Door")) {
+					Condition.wait(new Callable<Boolean>() {
+						@Override
+						public Boolean call() throws Exception {
+							return ctx.players.local().inMotion();
+						}
+					}, 50, 5);
+					Condition.wait(new Callable<Boolean>() {
+						@Override
+						public Boolean call() throws Exception {
+							return ctx.movement.distance(ctx.movement.destination(), ctx.players.local()) < Random.nextInt(2, 5) || ctx.chat.visible("Talk to the chef indicated.");
+						}
+					}, 200, 20);
+				}
+			} else {
+				Condition.sleep(400);
 			}
 			return;
 		}
@@ -109,7 +120,7 @@ public class MasterChef extends Talker {
 			}).poll();
 			door.bounds(OSTutorialIsland.BOUNDS_DOOR_EW);
 			if (ctx.camera.prepare(door) && door.click("Open")) {
-				if(Condition.wait(new Callable<Boolean>() {
+				if (Condition.wait(new Callable<Boolean>() {
 					@Override
 					public Boolean call() throws Exception {
 						return ctx.chat.visible("Now, how about showing some feelings?");
