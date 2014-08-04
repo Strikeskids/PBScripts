07dd3bfa2973fc3edec329c52ab683b52b00736d
Logicail <logicail@mike-nightingale.co.uk>
2014-08-04 16:36:48 +0100

    Remove rt4 item def reflection

--- src/com/logicail/loader/rt4/wrappers/ItemDefinition.java
+++ src/com/logicail/loader/rt4/wrappers/ItemDefinition.java
@@ -1,10 +1,14 @@
 package com.logicail.loader.rt4.wrappers;
 
 import com.logicail.loader.rt4.wrappers.loaders.ItemDefinitionLoader;
-import com.sk.cache.wrappers.ProtocolWrapper;
+import com.sk.cache.wrappers.StreamedWrapper;
 import com.sk.cache.wrappers.protocol.BasicProtocol;
 import com.sk.cache.wrappers.protocol.ProtocolGroup;
-import com.sk.cache.wrappers.protocol.extractor.*;
+import com.sk.cache.wrappers.protocol.extractor.ArrayExtractor;
+import com.sk.cache.wrappers.protocol.extractor.FieldExtractor;
+import com.sk.cache.wrappers.protocol.extractor.ParseType;
+import com.sk.cache.wrappers.protocol.extractor.StreamExtractor;
+import com.sk.datastream.Stream;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,7 +16,7 @@ import com.sk.cache.wrappers.protocol.extractor.*;
  * Date: 07/07/2014
  * Time: 10:37
  */
-public class ItemDefinition extends ProtocolWrapper {
+public class ItemDefinition extends StreamedWrapper {
 	private static final ProtocolGroup protocol = new ProtocolGroup();
 	public int team = 0;
 	public String name = null;
@@ -29,25 +33,15 @@ public class ItemDefinition extends ProtocolWrapper {
 
 	static {
 		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.BYTE)}, 113, 114).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(new StaticExtractor(true), "members")}, 16).addSelfToGroup(protocol);
 		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.USHORT), new FieldExtractor(ParseType.USHORT)}, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.USHORT, "modelId")}, 1).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.INT, "value")}, 12).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.STRING, "name")}, 2).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.USHORT, "noteId")}, 97).addSelfToGroup(protocol);
 		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.UINT24)}, 23, 25).addSelfToGroup(protocol);
 		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.USHORT)}, 4, 5, 6, 7, 8, 24, 26, 78, 79, 90, 91, 92, 93, 95, 110, 111, 112).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(new StaticExtractor(true), "stackable")}, 11).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.USHORT, "noteTemplateId")}, 98).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.STRING, "groundActions")}, 30, 31, 32, 33, 34).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.STRING, "actions")}, 35, 36, 37, 38, 39).addSelfToGroup(protocol);
-		new BasicProtocol(new FieldExtractor[]{new FieldExtractor(ParseType.UBYTE, "team")}, 115).addSelfToGroup(protocol);
 		new BasicProtocol(new FieldExtractor[]{new ArrayExtractor(ParseType.UBYTE, 0, new StreamExtractor[]{ParseType.USHORT, ParseType.USHORT}, null)}, 40, 41).addSelfToGroup(protocol);
 
 	}
 
 	public ItemDefinition(ItemDefinitionLoader loader, int id) {
-		super(loader, id, protocol);
+		super(loader, id);
 		this.loader = loader;
 	}
 
@@ -64,5 +58,35 @@ public class ItemDefinition extends ProtocolWrapper {
 		this.members = rhs.members;
 		this.value = rhs.value;
 		this.stackable = true;
+	}
+
+	@Override
+	public void decode(Stream s) {
+		int opcode;
+		while ((opcode = s.getUByte()) != 0) {
+			if (opcode == 16) {
+				members = true;
+			} else if (opcode == 1) {
+				modelId = s.getUShort();
+			} else if (opcode == 12) {
+				value = s.getInt();
+			} else if (opcode == 2) {
+				name = s.getString();
+			} else if (opcode == 97) {
+				noteId = s.getUShort();
+			} else if (opcode == 11) {
+				stackable = true;
+			} else if (opcode == 98) {
+				noteTemplateId = s.getUShort();
+			} else if (opcode >= 30 && opcode <= 34) {
+				groundActions[opcode - 30] = s.getString();
+			} else if (opcode >= 35 && opcode <= 39) {
+				actions[opcode - 30] = s.getString();
+			} else if (opcode == 115) {
+				team = s.getUByte();
+			} else {
+				protocol.read(this, opcode, s);
+			}
+		}
 	}
 }
