d641828536a20d926eac31cfdf615af629057ba1
Michael Nightingale <mike@mike-nightingale.co.uk>
2014-08-24 23:13:03 +0100

    RT6 script loader

--- src/org/logicail/cache/loader/rt6/wrapper/Script.java
+++ src/org/logicail/cache/loader/rt6/wrapper/Script.java
@@ -1,10 +1,55 @@
 package org.logicail.cache.loader.rt6.wrapper;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Logicail
- * Date: 15/08/2014
- * Time: 20:32
- */
-public class Script {
+import com.sk.cache.wrappers.StreamedWrapper;
+import com.sk.datastream.Stream;
+import org.logicail.cache.loader.rt6.wrapper.loaders.ScriptLoader;
+import org.powerbot.script.rt4.ClientContext;
+
+public class Script extends StreamedWrapper {
+	public static final int[] MASKS;
+
+	public int configId = -1;
+	public int configType = -1;
+	public int lowerBitIndex = -1;
+	public int upperBitIndex = -1;
+
+	static {
+		MASKS = new int[32];
+		int x = 2;
+		for (int i = 0; i < 32; i++) {
+			MASKS[i] = x - 1;
+			x += x;
+		}
+	}
+
+	public Script(ScriptLoader loader, int id) {
+		super(loader, id);
+	}
+
+	@Override
+	public String toString() {
+		return "Script " + id + " => " + code();
+	}
+
+	public String code() {
+		return "ctx.varpbits.varpbit(" + configId + ", " + lowerBitIndex + ", 0x" + Integer.toHexString(MASKS[upperBitIndex - lowerBitIndex]) + ")";
+	}
+
+	@Override
+	public void decode(Stream s) {
+		int opcode;
+		while ((opcode = s.getUByte()) != 0) {
+			if (opcode == 1) {
+				configType = s.getUByte();
+				configId = s.getBigSmart();
+			} else if (opcode == 2) {
+				lowerBitIndex = s.getUByte();
+				upperBitIndex = s.getUByte();
+			}
+		}
+	}
+
+	public int execute(ClientContext ctx) {
+		return ctx.varpbits.varpbit(configId) >> lowerBitIndex & MASKS[upperBitIndex - lowerBitIndex];
+	}
 }
