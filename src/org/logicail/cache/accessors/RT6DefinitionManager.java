6c8f37c33a78fa74685d67a852c8f72ed7558b36
Michael Nightingale <mike@mike-nightingale.co.uk>
2014-08-24 23:14:25 +0100

    Add some rt6 loaders

--- src/org/logicail/cache/accessors/RT6DefinitionManager.java
+++ src/org/logicail/cache/accessors/RT6DefinitionManager.java
@@ -1,13 +1,17 @@
 package org.logicail.cache.accessors;
 
+import com.sk.cache.DataSource;
 import org.logicail.cache.DefinitionCache;
 import org.logicail.cache.loader.rt6.RT6CacheSystem;
 import org.logicail.cache.loader.rt6.wrapper.ItemDefinition;
-import com.sk.cache.DataSource;
-import org.powerbot.script.rt4.Item;
+import org.logicail.cache.loader.rt6.wrapper.ObjectDefinition;
+import org.logicail.cache.loader.rt6.wrapper.QuestDefinition;
+import org.logicail.cache.loader.rt6.wrapper.Script;
+import org.logicail.cache.loader.rt6.wrapper.loaders.QuestDefinitionLoader;
 import org.powerbot.script.rt6.ClientAccessor;
 import org.powerbot.script.rt6.ClientContext;
 import org.powerbot.script.rt6.GroundItem;
+import org.powerbot.script.rt6.Item;
 
 import java.io.FileNotFoundException;
 
@@ -19,6 +23,9 @@ import java.io.FileNotFoundException;
  */
 public class RT6DefinitionManager extends ClientAccessor {
 	private final DefinitionCache<ClientContext, ItemDefinition> item;
+	private final DefinitionCache<ClientContext, ObjectDefinition> object;
+	private final DefinitionCache<ClientContext, Script> script;
+	private final QuestDefinitionLoader questLoader;
 	private final RT6CacheSystem system;
 
 	public RT6CacheSystem system() {
@@ -39,10 +46,25 @@ public class RT6DefinitionManager extends ClientAccessor {
 		system = new RT6CacheSystem(DataSource.getDefaultCacheDirectory("runescape"));
 
 		item = new DefinitionCache<ClientContext, ItemDefinition>(this.ctx, system.itemLoader);
+		object = new DefinitionCache<ClientContext, ObjectDefinition>(this.ctx, system.objectLoader);
+		script = new DefinitionCache<ClientContext, Script>(this.ctx, system.scriptLoader);
+		questLoader = new QuestDefinitionLoader(system);
 
 	}
 
 	public ItemDefinition item(int id) {
 		return item.get(id);
+	}
+
+	public ObjectDefinition object(int id) {
+		return object.get(id);
+	}
+
+	public QuestDefinition quest(String name) {
+		return questLoader.find(name);
+	}
+
+	public Script script(int id) {
+		return script.get(id);
 	}
 }
